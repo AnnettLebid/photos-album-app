7861931a0c5f419515f61499d929d9a0d0d54640
Anna Lebid <annalez16@gmail.com>
2023-12-21 00:26:43 +0200

    update NavBar component, add img

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,26 @@
 <template>
-  <v-toolbar app dark color="purple-accent-4 text-white">
-    <v-toolbar-title>Photos Album</v-toolbar-title>
-
-    <RouterLink to="/">
-      <v-btn class="text-none text-subtitle-1" color="white">Home</v-btn>
-    </RouterLink>
-    <RouterLink to="/favorite">
-      <v-btn class="text-none text-subtitle-1" color="white"
-        >Favorites</v-btn
-      ></RouterLink
-    >
+  <v-toolbar app dark color="purple-accent-4 text-white px-2">
+    <v-toolbar-title class="hidden-xs">Photos Album</v-toolbar-title>
+    <div>
+      <img
+        src="@/assets/gallery.svg"
+        alt="Not Found"
+        class="content-center hidden-sm-and-up"
+      />
+    </div>
+    <div class="ml-auto">
+      <RouterLink to="/">
+        <v-btn class="text-none text-subtitle-1" color="white" flat>Home</v-btn>
+      </RouterLink>
+      <RouterLink to="/favorite">
+        <v-btn class="text-none text-subtitle-1" color="white" flat
+          >Favorites</v-btn
+        >
+      </RouterLink>
+    </div>
   </v-toolbar>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
 </script>
-
-<style lang="scss" scoped></style>
