2a5788a436c38633d4b1b1139f1a74bc7d3cb923
Anna Lebid <annalez16@gmail.com>
2023-12-20 09:39:56 +0200

    add navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar app dark color="purple-accent-4 text-white">
+    <v-toolbar-title>Photos Album</v-toolbar-title>
+
+    <RouterLink to="/">
+      <v-btn class="text-none text-subtitle-1" color="white">Home</v-btn>
+    </RouterLink>
+    <RouterLink to="/favorite">
+      <v-btn class="text-none text-subtitle-1" color="white"
+        >Favorites</v-btn
+      ></RouterLink
+    >
+  </v-toolbar>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
