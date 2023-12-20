e904c676d7e20ead5e6c530c9378e3a5d246ef21
Anna Lebid <annalez16@gmail.com>
2023-12-20 22:58:55 +0200

    add ImagesList component

---
+++ src/components/ImagesList.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="image in albumImages"
+      :key="image.id"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      <v-img
+        :src="image.url"
+        :lazy-src="image.thumbnailUrl"
+        aspect-ratio="1"
+        cover
+        class="bg-grey-lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey-lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+        <v-btn flat color="pink" id="no-background-hover">
+          <v-icon>mdi-heart-outline</v-icon>
+        </v-btn>
+      </v-img>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+defineProps(["albumImages"]);
+</script>
