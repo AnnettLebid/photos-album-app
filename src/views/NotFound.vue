04369a5f17b3f1a5c854bfa656bebbb0c4005702
Anna Lebid <annalez16@gmail.com>
2023-12-20 09:41:06 +0200

    add NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>404</h1>
+        <h2>UH OH! You're lost.</h2>
+        <p>
+          The page you are looking for does not exist. How you got here is a
+          mystery. But you can click the button below to go back to the
+          homepage.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
