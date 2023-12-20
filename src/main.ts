cadeb804f00bec3c1b4d0d56e32f3a957f7d0ad1
Anna Lebid <annalez16@gmail.com>
2023-12-20 22:58:11 +0200

    update createVuetify

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "vuetify/styles"
+import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import "@mdi/font/css/materialdesignicons.css";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import { mdi, aliases } from "vuetify/iconsets/mdi";
+
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
   components,
   directives,
 });
@@ -16,4 +28,4 @@ const app = createApp(App);
 
 app.use(vuetify);
 app.use(router);
-app.mount("#app")
+app.mount("#app");
