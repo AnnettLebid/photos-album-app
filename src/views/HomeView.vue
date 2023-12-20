163bde145be0ede14cbfee1e47fda5c62933d899
Anna Lebid <annalez16@gmail.com>
2023-12-20 20:48:18 +0200

    add HomeView component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useApi } from "../composables/useApi";
+import { BASE_URL } from "../helpers/constants";
+import AlbumList from "../components/AlbumsList.vue";
+import router from "../router";
+
+const { data, loading, error } = useApi(BASE_URL);
+
+const showAlbum = (id: string) => {
+  router.push(`/albums/${id}`);
+};
+</script>
+
+<template>
+  <v-container>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">Error: {{ error }}</div>
+    <AlbumList v-if="data.length > 0" :albums="data" @showAlbum="showAlbum" />
+  </v-container>
+</template>
