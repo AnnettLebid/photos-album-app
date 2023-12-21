57119233dd99f334501d96f5c041a98f2a895b55
Anna Lebid <annalez16@gmail.com>
2023-12-21 11:54:27 +0200

    add interfaces for Album and Photo

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,50 @@
 <script setup>
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
 
+<template>
+  <!-- <v-text-field
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    type="input"
+    density="compact"
+    variant="solo"
+    append-inner-icon="mdi-magnify single-line hide-details"
+  > -->
+
+  <v-container>
+    <v-card>
+      <input
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <!-- <v-text-field
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    > -->
+      <!-- <input
+        type="text"
+        placeholder="Search photos..."
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        class="m-20"
+      /> -->
+      <!-- </v-text-field> -->
+    </v-card>
+  </v-container>
+  <!-- </v-text-field> -->
+</template>
+
+<!-- <script setup>
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
 </script>
 
 <template>
   <v-text-field
+    :value="searchValue"
+    @input="$emit('update:searchValue', $event.target.value)"
     :loading="loading"
     density="compact"
     variant="solo"
@@ -11,6 +52,6 @@
     append-inner-icon="mdi-magnify"
     single-line
     hide-details
-    @click:append-inner="onClick"
-  ></v-text-field>
-</template>
+  />
+  <p>Message is: {{ searchValue }}</p>
+</template> -->
