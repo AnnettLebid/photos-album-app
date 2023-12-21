0f8aa58487f72e8e72703213beaa0ed000816338
Anna Lebid <annalez16@gmail.com>
2023-12-21 15:23:37 +0200

    update ImagesList

--- src/components/ImagesList.vue
+++ src/components/ImagesList.vue
@@ -1,36 +1,56 @@
-<script setup>
+<script setup lang="ts">
 import { defineProps, defineEmits } from "vue";
 
-defineProps(["albumImages"]);
+const emit = defineEmits(["addFavorite"]);
+const props = defineProps(["albumImages"]);
+
+const handleFavorite = (image: number) => {
+  emit("addFavorite", image);
+};
+console.log("albumImages", props);
 </script>
 
 <template>
-  <v-row>
-    <v-col
-      v-for="image in albumImages"
-      :key="image.id"
-      class="d-flex child-flex"
-      cols="4"
-    >
-      <v-img
-        :src="image.url"
-        :lazy-src="image.thumbnailUrl"
-        aspect-ratio="1"
-        cover
-        class="bg-grey-lighten-2"
+  <v-container mt-2>
+    <v-row>
+      <v-col
+        v-for="image in albumImages"
+        :key="image.id"
+        class="d-flex child-flex"
+        cols="4"
       >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey-lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-        <v-btn flat color="pink" id="no-background-hover">
-          <v-icon>mdi-heart-outline</v-icon>
-        </v-btn>
-      </v-img>
-    </v-col>
-  </v-row>
+        <v-img
+          :src="image.url"
+          :lazy-src="image.thumbnailUrl"
+          aspect-ratio="1"
+          cover
+          class="bg-grey-lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey-lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+          <div>
+            <v-btn
+              size="small"
+              color="pink"
+              variant="text"
+              icon="mdi-heart"
+            ></v-btn>
+            <v-btn
+              @click="handleFavorite(image)"
+              size="small"
+              color="pink"
+              variant="text"
+              icon="mdi-heart-outline"
+            ></v-btn>
+          </div>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
