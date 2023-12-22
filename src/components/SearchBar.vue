909407de58d2351f978c316b34c92e6ca5307ab9
Anna Lebid <annalez16@gmail.com>
2023-12-22 10:29:15 +0200

    fix SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,57 +1,22 @@
 <script setup>
+import { defineProps, defineEmits } from "vue";
 defineProps(["modelValue"]);
 defineEmits(["update:modelValue"]);
 </script>
 
 <template>
-  <!-- <v-text-field
-    :value="modelValue"
-    @input="$emit('update:modelValue', $event.target.value)"
-    type="input"
-    density="compact"
-    variant="solo"
-    append-inner-icon="mdi-magnify single-line hide-details"
-  > -->
-
   <v-container>
-    <v-card>
-      <input
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-      />
-      <!-- <v-text-field
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-    > -->
-      <!-- <input
-        type="text"
-        placeholder="Search photos..."
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        class="m-20"
-      /> -->
-      <!-- </v-text-field> -->
-    </v-card>
+    <v-row>
+      <v-col cols="12">
+        <input
+          type="text"
+          id="search"
+          placeholder="Search photos..."
+          class="w-100 px-2 py-1 rounded border border-primary box-shadow"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+        />
+      </v-col>
+    </v-row>
   </v-container>
-  <!-- </v-text-field> -->
 </template>
-
-<!-- <script setup>
-defineProps(["modelValue"]);
-defineEmits(["update:modelValue"]);
-</script>
-
-<template>
-  <v-text-field
-    :value="searchValue"
-    @input="$emit('update:searchValue', $event.target.value)"
-    :loading="loading"
-    density="compact"
-    variant="solo"
-    label="Search photos..."
-    append-inner-icon="mdi-magnify"
-    single-line
-    hide-details
-  />
-  <p>Message is: {{ searchValue }}</p>
-</template> -->
