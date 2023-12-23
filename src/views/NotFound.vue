f39482b3bd829bc777be8a406ab180de7a7d2ba4
Anna Lebid <annalez16@gmail.com>
2023-12-23 23:34:06 +0200

    add translations to NotFound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
   <v-container fill-height>
     <v-row justify="center" align="center" smAndUp:margin-t-left>
@@ -8,15 +12,17 @@
           class="content-center"
         />
       </v-col>
-      <v-col cols="12" md="3" class="xsOnly:text-center smAndUp:text-left">
+      <v-col cols="12" md="3" class="text-center mdAndUp:text-left">
         <h1>404</h1>
-        <h2>UH OH! You're lost.</h2>
+        <h2>{{ $t("not_found.title") }}</h2>
         <p class="mb-3">
-          The page you are looking for does not exist. How you got here is a
-          mystery. But you can click the button below to go back to the
-          homepage.
+          {{ $t("not_found.message") }}
         </p>
-        <v-btn color="teal-accent-4 white--text align">HOME</v-btn>
+        <RouterLink to="/">
+          <v-btn color="teal-accent-4 white--text align">{{
+            $t("not_found.home_button")
+          }}</v-btn>
+        </RouterLink>
       </v-col>
     </v-row>
   </v-container>
